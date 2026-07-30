3ef58a65f1409907cd92e818abe910d2a6c5d6de
patil-vaishnavii <vaishnaviipatil2212@gmail.com>
2026-07-30 14:14:46 +0530

    added sign up and login features.

--- FoodVerse/urls.py
+++ FoodVerse/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("restaurants.urls")),
+    path("",include("accounts.urls")),
 ]
 
 if settings.DEBUG:
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate,login,logout
 
-# Create your views here.
+def register(request):
+
+  if request.method == "POST":
+
+    form = RegistrationForm(request.POST)
+
+    if form.is_valid():
+
+      user = form.save()
+      login(request,user)
+      return redirect("/")
+
+  else:
+    form = RegistrationForm()
+
+  return render(
+    request, "accounts/register.html",
+    {
+      "form":form
+    }
+  )
+
+def login_view(request):
+
+  if request.method == "POST":
+
+    form = AuthenticationForm(request, data=request.POST)
+
+    if form.is_valid():
+
+      username = form.cleaned_data.get("username") # cleaned_data contains the validated values from the form.
+      password = form.cleaned_data.get("password")
+
+      user = authenticate(
+        request,
+        username=username,
+        password=password
+      )
+
+      if user is not None:
+        login(request,user) #Remember this user for future requests.
+
+        return redirect("/")
+      
+  else: 
+    form = AuthenticationForm()
+
+  return render(
+    request,
+    "accounts/login.html",
+    {
+      "form": form
+    }
+  )
+
+def logout_view(request):
+  logout(request)
+  return redirect("/")
