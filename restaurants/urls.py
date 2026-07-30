0651eac4e4341af02fe29aa5d7a062d9c563d016
patil-vaishnavii <vaishnaviipatil2212@gmail.com>
2026-07-30 16:30:45 +0530

    Clean duplicate folders after project restructure

--- restaurants/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("",views.restaurant_list,name="restaurant_list"),
-
-    path('restaurant/<int:id>/',views.restaurant_detail,name='restaurant_detail'),
-]
